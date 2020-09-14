71baac839691593c1fe5923772e7686179cdd05f
unknown <mircosteyer@web.de>
2020-09-14 16:14:44 +0200

    uibuild

--- index.js
+++ index.js
@@ -65,21 +65,17 @@ app.delete("/api/notes/:id", (req, res) => {
     res.status(204).end()*/
 })
 
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res, next) => {
     const body = req.body
-
-    if (!body.content) {
-        return res.status(400).json({error: "No content given."})
-    }
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
     })
-    note.save().then((response) => {
-        res.json(response)})
-/*        .catch(error => error.status(400).json({error: "An error occurred."}))*/
+    note.save()
+        .then(response => {res.json(response)})
+        .catch(error => next(error))
 })
 
 app.put("/api/notes/:id", (req,res) => {
@@ -104,7 +100,10 @@ app.use(unknownEndpoint)
 const errorHandler = (error, req, res, next) => {
     console.log(error.message)
     if (error.name === "CastError") {
-        return res.status(404).send({error: "malformatted id"})
+        return res.status(400).send({error: "malformatted id"})
+    }
+    if (error.name === "ValidationError") {
+        return res.status(400).send({error: error.message})
     }
     next(error)
 }
--- models/note.js
+++ models/note.js
@@ -9,8 +9,15 @@ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     .catch(error => {console.log("Error: ", error)})
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: {
+        type: String,
+        minlength: 5,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
     important: Boolean,
 }, {
     toJSON: {
